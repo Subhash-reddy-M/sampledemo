d4dcebe0077666d68a32535510f445f093cf31a7
Subhash-reddy-M <msubhash8688@gmail.com>
2025-01-28 12:33:27 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,16 @@
+'''
+1.single line
+2.multiline comments
+'''
+
+# bellow statement will print something
+
+print/("#single line comments#")
+print("//multi line comments//")
+      
+
+
+
+
+
+print("python life")
